631fe0a94d3bf58e2c69b3cc10e6b1475a7a718c
Weston Wrzesinski <wwrzesin@asu.edu>
2020-03-09 16:46:46 -0700

    Step 6

---
+++ japi.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+
+APIKey = "VPNQRTV7WSSWRFWK"
+
+def getStockData(symbol):
+    try:
+        print("Looking up symbol...")
+        timeSeries = TimeSeries(key=APIKey,output_format='pandas')
+        data, meta_data = timeSeries.get_intraday(symbol=symbol,interval='1min')
+        return str(data.tail(1).iloc[0]['4. close'])
+    except:
+        return "Symbol Not Found"
+
+def main():
+    while 1:
+        userinput = input("Enter stock symbol or type QUIT to quit: ").upper()
+        if userinput != "QUIT":
+            serverinfo = "The current price of {} is {}\n".format(userinput, getStockData(userinput))
+            print(serverinfo)
+            f = open('japi.out', 'a')
+            f.write("\n"+serverinfo)
+        else:
+            f.close()
+            exit()
+
+main()
